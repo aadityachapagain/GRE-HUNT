043b109e51a1ed84f05e50321dda189255b30d28
aadityachapagain <chapagain.aaditya@gmail.com>
2019-06-18 21:04:44 +0545

    add usage and example query for words

--- grehunt.sh
+++ grehunt.sh
@@ -24,10 +24,55 @@ wrong_command()
             "
 }
 
+append_usage()
+{
+    echo " --apend usage pattern 
+    
+            [--append | -a ] <Word> \"<meaning>\" [optional] --example  \"<write all the example of using given word>\"
+             --usage \"<write the use of word in manykind>\"
+            
+            "
+}
+
 if [ $# -gt 0 ]; then
     case $1 in
         -a | --append )  shift
-                         python app.py append $1 $2
+                         if [ $# -gt 0 ]; then
+
+                            arg1="${1// /_}"
+                         else
+                            append_usage
+                            exit
+                         fi
+                         shift
+                         if [ $# -gt 0 ]; then
+                            arg2="${1// /_}"
+                         else
+                            append_usage
+                            exit
+                         fi
+
+                         shift
+
+                         if [ "$1" == "--example" ]; then
+                            shift
+                            arg3="${1// /_}"
+                            shift
+                            if [ "$1" == "--usage" ]; then
+                                shift
+                                arg4="${1// /_}"
+                                shift
+                                if [ $# -gt 0 ]; then
+                                    append_usage
+                                fi
+                                python app.py append $arg1 $arg2 $arg3 $arg4
+                             else
+                                 python app.py append $arg1 $arg2 $arg3
+                            fi
+
+                         else
+                            python app.py append $arg1 $arg2
+                         fi
                          ;;
 
         list )           shift
@@ -41,7 +86,6 @@ if [ $# -gt 0 ]; then
         --help | -h )    usage
                          ;;
         * )              wrong_command
-                         ;;
                          
     esac
 else
