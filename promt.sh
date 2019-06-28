752d8261954c8def874d0f60c85dde88945052a7
aadityachapagain <chapagain.aaditya@gmail.com>
2019-06-28 15:09:12 +0545

    add notification prompt to alert user about revision hour

---
+++ promt.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# make sure you gave a number of seconds:
+
+
+description=$(cat <<-END
+    Hello,
+
+    I think its time you better revise the words in database or
+    may be update database with new words.
+
+    Bonjour !
+
+END
+)
+
+while true; do
+    notify-send "Revision" "$description" --urgency="low" --expire-time=4
+
+    sleep 3600
+done
