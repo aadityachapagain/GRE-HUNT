2f310afb35fca0272166b7f6156f2cacc044ca5e
aadityachapagain <chapagain.aaditya@gmail.com>
2019-07-11 09:55:50 +0545

    update system notification with sounds and icon in it

--- system_notify.py
+++ system_notify.py
@@ -1,9 +1,15 @@
+# python packages
 import sys
 import os
 import datetime
 import time
 import json
 import gi
+
+# project packages
+from config.setting import ICON_PATH, MUSIC_PATH, APP_NAME
+from core import get_random_record, display_format
+
 gi.require_version('Notify', '0.7')
 from gi.repository import Notify
 gi.require_version('GdkPixbuf', '2.0')
@@ -11,13 +17,7 @@ from gi.repository import GdkPixbuf
 
 
 def playbeep():
-    os.system('paplay /opt/never_kill_your_time/Xylo.ogg')
-
-
-def timenow():
-    now = datetime.datetime.now()
-    time = now.strftime('%H:%M')
-    return time
+    os.system(f'paplay {MUSIC_PATH}')
 
 
 def showNotification(title, body, image):
@@ -30,59 +30,12 @@ def showNotification(title, body, image):
     notification.show()
     playbeep()
 
-data = None
-with open('/opt/never_kill_your_time/routine.json') as f:
-    data = json.loads(f.read())
-
-
 def main():
     now = datetime.datetime.now()
     Notify.init('never kill your time')
-    image = GdkPixbuf.Pixbuf.new_from_file(
-        '/opt/never_kill_your_time/notification.png')
-
-    while True:
-        for routine in data:
-            if routine['time'] == timenow():
-                showNotification(routine['title'], routine['body'], image)
-
-        time.sleep(60)
-
-
-def add(x, y, z):
-    info = {
-        'time': x,
-        'title': y,
-        'body': z
-    }
-    data.append(info)
-    with open('/opt/never_kill_your_time/routine.json', 'w') as outfile:
-        json.dump(data, outfile)
-
-
-def update(x, y, z):
-    for routine in data:
-        if routine['time'] == x:
-            routine['title'] = y
-            routine['body'] = z
-    with open('/opt/never_kill_your_time/routine.json', 'w') as outfile:
-        json.dump(data, outfile)
-
-
-def delete(x):
-    for routine_i in data:
-        if routine_i['time'] == x:
-            data.remove(routine_i)
-    with open('/opt/never_kill_your_time/routine.json', 'w') as outfile:
-        json.dump(data, outfile)
+    image = GdkPixbuf.Pixbuf.new_from_file(f'{ICON_PATH}')
+    for docs in get_random_record(1):
+        showNotification(APP_NAME, display_format(docs), image)
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if sys.argv[1] == 'add':
-            add(sys.argv[2], sys.argv[3], sys.argv[4])
-        elif sys.argv[1] == 'update':
-            update(sys.argv[2], sys.argv[3], sys.argv[4])
-        elif sys.argv[1] == 'delete':
-            delete(sys.argv[2])
-    else:
         main()
