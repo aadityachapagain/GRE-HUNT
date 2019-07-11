59aa24592bbad8efabab7f896f9fc33715513ee5
aadityachapagain <chapagain.aaditya@gmail.com>
2019-07-11 09:39:18 +0545

    add settings to store static project files

---
+++ config/setting.py
@@ -0,0 +1,10 @@
+from os.path import abspath, dirname, join
+import os
+
+# paths for various project folder
+ROOT_DIR = dirname(dirname(abspath(__file__)))
+DATA_DIR = join(ROOT_DIR, 'data')
+
+ICON_PATH = join(DATA_DIR, 'icon.png')
+
+MUSIC_PATH = join(DATA_DIR,'alert.ogg')
